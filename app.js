3818c6ed58ad070046ff8a069e04381f98804f2c
Surface <da.ga.mo.94@gmail.com>
2019-06-18 15:15:27 -0600

    directions added

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+
+var address = "";
+
+var directionsService;
+var directionsDisplay;
 
 $('button').click((event) => {
     event.preventDefault();
@@ -63,6 +71,100 @@ $('button').click((event) => {
         }
         $("#yelpResultsBody").on("click", ".newrow", function () {
             console.log($(this).attr("data"));
+            address = $(this).attr("data");
+            console.log(address);
+
+            var onChangeHandler = function () {
+                calculateAndDisplayRoute(directionsService, directionsDisplay);
+            };
+
+            onChangeHandler();
+
         })
     })
 })
+
+// import { POINT_CONVERSION_COMPRESSED } from "constants";
+var myPosition;
+function initMap() {
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer({
+        draggable:false,
+        panel: document.getElementById("directions")
+
+    });
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: { lat: 39.7392, lng: -104.9903 }
+    });
+    console.log(map);
+    var geocoder = new google.maps.Geocoder;
+    var infoWindow = new google.maps.InfoWindow;
+    directionsDisplay.setMap(map);
+
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            position = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+
+            };
+            myPosition = position
+            console.log(position.lat);
+            console.log(position.lng);
+            infoWindow.setPosition(position);
+            infoWindow.setContent('Location found.' + position.lat + position.lng);
+            infoWindow.open(map);
+            map.setCenter(position);
+
+            console.log(position);
+
+            // var onChangeHandler = function () {
+            //     calculateAndDisplayRoute(directionsService, directionsDisplay);
+            // };
+
+            // // geocodeLatLng(geocoder, map, infoWindow);
+            // onChangeHandler();
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, position) {
+        infoWindow.setPosition(position);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Please input your location above' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
+
+
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+        origin: { lat: myPosition.lat, lng: myPosition.lng },
+        destination: address,
+        travelMode: 'WALKING'
+    }, function (response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+        directionsDisplay.getDirections;
+    });
+    console.log("start: ", myPosition);
+    // console.log(destination);
+
+}
+
+
+
